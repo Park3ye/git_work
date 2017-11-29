1252a43df75afd71e6a81813bf706764ed85e8b2
Park3ye <psw0305@naver.com>
2017-11-30 00:11:25 +0900

    현재까지과정들

---
+++ step_01/js/js_basic_07.js
@@ -0,0 +1,14 @@
+//  js_basic_07.js
+/**/
+var hello = 'hello js';
+
+console.log('hello js');
+
+console.log(hello);
+
+var day = 0;
+for(; day <= 1000; day++){
+  if(day%7 == 0){
+    console.log('day is:'+ day, '현정이에게 사탕주기');
+  }
+}
